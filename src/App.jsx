4962804c4098d193bb7808203a5b5e7f7e87e9f7
jaytiwari2004 <jaytiwari55798@gmail.com>
2025-12-05 17:34:34 +0530

    changes one

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { HelmetProvider } from "react-helmet-async";
+
 import Home from "./pages/Home";
 
 export default function App(){
   return (
-    <HelmetProvider>
+    
       <Home />
-    </HelmetProvider>
+      
+    
+    
   );
 }
