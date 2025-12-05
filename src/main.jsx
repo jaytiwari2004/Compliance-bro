29f2f66d5103f71bdd91082d90718c3e0fa19377
jaytiwari2004 <jaytiwari55798@gmail.com>
2025-12-05 17:22:04 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
+
 import App from "./App.jsx";
 import "./index.css";
 
@@ -10,4 +10,5 @@ ReactDOM.createRoot(document.getElementById("app")).render(
       <App />
     </React.StrictMode>
   </HelmetProvider>
+  
 );
